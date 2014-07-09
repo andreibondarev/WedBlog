ad737750940679507d6d57304555e147bc72130d
Greg Loesch <gloesch@weddingwire.com>
2014-07-09 14:56:28 -0400

    Create an adapter to display the posts [session-5]

--- app/src/main/java/com/weddingwire/wedblog/activities/PostListActivity.java
+++ app/src/main/java/com/weddingwire/wedblog/activities/PostListActivity.java
@@ -2,15 +2,31 @@ package com.weddingwire.wedblog.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.weddingwire.wedblog.R;
+import com.weddingwire.wedblog.WBApplication;
+import com.weddingwire.wedblog.models.Post;
+
+import java.util.List;
 
 public class PostListActivity extends Activity {
+
+    private ListView mPostsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
+        mPostsList = (ListView) findViewById(R.id.post_list);
+        setupList();
+    }
+
+    private void setupList() {
+        List<Post> posts = ((WBApplication)getApplication()).getPosts();
+        ArrayAdapter<Post> adapter = new ArrayAdapter<Post>(this, android.R.layout.simple_list_item_1, posts);
+        mPostsList.setAdapter(adapter);
     }
 
 }
