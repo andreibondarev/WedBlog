6de7bcdaee3ea69d06e3b0cb882c2371d97cb4f5
Greg Loesch <gloesch@weddingwire.com>
2014-07-09 14:53:57 -0400

    Add functionality to create post form [session-5]
    - Add create post form functionality.
    - Enhance the experience of the form.

--- app/src/main/java/com/weddingwire/wedblog/activities/CreatePostActivity.java
+++ app/src/main/java/com/weddingwire/wedblog/activities/CreatePostActivity.java
@@ -2,15 +2,76 @@ package com.weddingwire.wedblog.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.weddingwire.wedblog.R;
+import com.weddingwire.wedblog.WBApplication;
+import com.weddingwire.wedblog.models.Post;
 
 public class CreatePostActivity extends Activity {
 	private final static String TAG = CreatePostActivity.class.getSimpleName();
+
+    private EditText mTitle;
+    private EditText mAuthor;
+    private EditText mBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        inject();
+        setupButton();
+    }
+
+    private void inject() {
+        mTitle = (EditText) findViewById(R.id.title);
+        mAuthor = (EditText) findViewById(R.id.author);
+        mBody = (EditText) findViewById(R.id.body);
+    }
+
+    private void setupButton() {
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (valid()) {
+                    Post post = new Post();
+                    post.setTitle(mTitle.getText().toString());
+                    post.setAuthorName(mAuthor.getText().toString());
+                    post.setBody(mBody.getText().toString());
+
+                    Log.d(TAG, post.toString());
+                    addPost(post);
+
+                    Toast successToast = Toast.makeText(CreatePostActivity.this, "Successful post!", Toast.LENGTH_SHORT);
+                    successToast.show();
+
+                    mTitle.getText().clear();
+                    mAuthor.getText().clear();
+                    mBody.getText().clear();
+
+                    mTitle.requestFocus();
+                }
+            }
+        });
+    }
+
+    private boolean valid() {
+        boolean valid = true;
+        if (mTitle.getText().length() == 0) {
+            mTitle.setError("Title cannot be blank!");
+            mTitle.requestFocus();
+            valid = false;
+        }
+        return valid;
+    }
+
+    private void addPost(Post post) {
+        ((WBApplication)getApplication()).addPost(post);
     }
 }
